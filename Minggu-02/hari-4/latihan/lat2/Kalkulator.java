4b284aadba814290ff0782eb195fb3c4020bb75d
Muhammadakbaar <mohammadakbaar11@gmail.com>
2019-06-27 16:32:14 +0700

    update minggu kedua hari keempat

---
+++ Minggu-02/hari-4/latihan/lat2/Kalkulator.java
@@ -0,0 +1,18 @@
+public class Kalkulator {
+    
+    public double kali(double operandA,double operandB){
+        return operandA*operandB;
+    }
+    
+    public double bagi(double operandA,double operandB){
+        return operandA/operandB;
+    }
+    
+    public double tambah(double operandA,double operandB){
+        return operandA+operandB;
+    }
+    
+    public double kurang(double operandA,double operandB){
+        return operandA-operandB;
+    }   
+}
