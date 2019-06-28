6fdcf5d486c79953a0ad096bdd79ec979bee5b2c
Muhammadakbaar <mohammadakbaar11@gmail.com>
2019-06-28 16:15:44 +0700

    update minggu kedua hari kelima

---
+++ Minggu-02/hari-5/latihan/materi-1/HelloThread.java
@@ -0,0 +1,14 @@
+
+public class HelloThread extends Thread {
+
+    public static void main(String args[]) {
+        (new HelloThread()).start();
+    }
+
+    public void run() {
+        System.out.println("Hello from a thread!");
+    }
+
+    
+
+}
---
+++ Minggu-02/hari-5/latihan/materi-3/MyRunnable2.java
@@ -0,0 +1,18 @@
+
+
+public class MyRunnable2 implements Runnable {
+    private final long countUntil;
+
+    MyRunnable2(long countUntil) {
+        this.countUntil = countUntil;
+    }
+
+    @Override
+    public void run() {
+        long sum = 0;
+        for (long i = 1; i < countUntil; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
